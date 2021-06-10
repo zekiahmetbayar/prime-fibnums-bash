21854d40b8d821ddb1eafb87e8fd571cf847c6bb
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2021-06-10 17:16:26 +0300

    some divide functions added

---
+++ main.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+echo "Enter max value : (Max : 4.100.200.300)"
+read maxValue
+
+
+checkDivideByTwo(){
+    lastDigit=${maxValue: -1}
+    if [ $((lastDigit%2)) -eq 0 ] 
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
+checkDivideByThree(){
+    num=$maxValue
+    while [[ $num -gt 0 ]]
+    do
+            let temp=$num%10
+            let sum=$sum+$temp
+            let num=$num/10
+    done
+
+    if [ $((sum%3)) -eq 0 ] 
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
+checkDivideByFour(){
+    numberOfDigits="${#maxValue}"
+    
+    if [ "$numberOfDigits" -gt "2" ]; then
+        lastTwoDigit=${maxValue: -2}
+        if [ $lastTwoDigit -eq "00" ] | [ $((lastTwoDigit%4)) -eq 0 ];then
+            return 1
+        else
+            return 0
+        fi
+    else
+        lastDigit=${maxValue: -1}
+        if [ $((lastDigit%4)) -eq 0 ] 
+        then
+            return 1
+        else
+            return 0
+        fi
+    fi
+}
+
+checkDivideByFive(){
+    lastDigit=${maxValue: -1}
+    if [ $lastDigit -eq 0 ]
+    then
+        return 1
+    else
+        if [ $lastDigit -eq 5 ]
+        then
+            return 1
+        else
+            return 0
+        fi
+    fi
+}
+
+checkDivideBySix(){
+    checkDivideByTwo
+    local isDivisibleByTwo=$?
+
+    checkDivideByThree
+    local isDivisibleByThree=$?
+    
+    if [ $isDivisibleByTwo -eq 1 ] && [ $isDivisibleByThree -eq 1 ];then
+        return 1
+    else
+        return 0
+    fi
+}
+
+checkDivideByThree
+echo $?
